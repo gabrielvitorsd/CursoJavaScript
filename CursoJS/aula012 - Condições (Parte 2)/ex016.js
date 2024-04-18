83862d47dcd0a9963ff8e01f70a47181c7789957
gabrielvitorsd <163777402+gabrielvitorsd@users.noreply.github.com>
2024-04-18 17:36:48 -0300

    Add files via upload

---
+++ CursoJS/aula012 - Condições (Parte 2)/ex016.js
@@ -0,0 +1,8 @@
+//Condição composta if/else 
+var idade = 1
+if (idade > 18){
+    console.log('Menor de idade') //Console.log serva para imprimir na tela do Terminal em JavaScript. 
+    //Já window.document.write serve para imprimir na tela em JavaScript quando tem o navegador.
+}else {
+    console.log('Maior de idade')
+}
