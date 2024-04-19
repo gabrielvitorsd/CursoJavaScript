d2cc9ad3af6c172becafb753ff2a2b1f167a510c
gabrielvitorsd <163777402+gabrielvitorsd@users.noreply.github.com>
2024-04-19 15:59:07 -0300

    Delete CursoJS/aula012 - Condições (Parte 2) directory

--- CursoJS/aula012 - Condições (Parte 2)/ex018.js
+++
@@ -1,9 +0,0 @@
-//Condição composta (aninhada) if/else if
-var idadevoto = 25
-if (idadevoto < 16){
-    console.log('Não vota')
-}else if (idadevoto < 18){
-    console.log('Voto opcional')
-}else if (idadevoto >= 18){
-    console.log('Voto obrigatório')
-}
