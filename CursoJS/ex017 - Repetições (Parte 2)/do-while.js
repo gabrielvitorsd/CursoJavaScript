d126c5875f74cc62685b746cea40ab1dee03c990
gabrielvitorsd <163777402+gabrielvitorsd@users.noreply.github.com>
2024-04-25 15:26:41 -0300

    Delete CursoJS/ex017 - Repetições (Parte 2) directory

--- CursoJS/ex017 - Repetições (Parte 2)/do-while.js
+++
@@ -1,5 +0,0 @@
-var c = 1
-do {
-    console.log(c)
-    c = c + 1 //Vale lembrar que c = c + 1 é igual c ++.
-} while (c <= 10)
--- CursoJS/ex017 - Repetições (Parte 2)/while.js
+++
@@ -1,5 +0,0 @@
-var c = 1
-while (c <= 10){
-    console.log(c)
-    c = c + 1 //Vale lembrar que c = c + 1 é igual c ++.
-}
